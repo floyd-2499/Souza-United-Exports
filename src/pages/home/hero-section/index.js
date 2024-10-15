67561b45bdbe8c4ef8ba2fc8d385e0e3f8299d02
floyd-esgds <floyd.dsouza@esgds.io>
2024-10-15 20:22:04 +0530

    Fix resize & hero images

--- src/pages/home/hero-section/index.js
+++ src/pages/home/hero-section/index.js
@@ -4,6 +4,11 @@ import Image from "next/image"
 
 import styles from "./styles.module.scss";
 import HeroKitchenCountertop from "./assets/hero-kitchen-countertop.png"
+import Countertops from "./assets/Countertops.png"
+import Designer from "./assets/Designer.png"
+import Outdoor from "./assets/Outdoor.png"
+import Bathroom from "./assets/Bathroom.png"
+import Exotic from "./assets/Exotic.png"
 import NetBg from "../../../assets/Net-BG.svg"
 import classNames from "classnames";
 import { uris } from "@/library/uris";
@@ -12,43 +17,43 @@ import { useRouter } from "next/router";
 const carouselContents = [
     {
         id: 1,
-        title: "Elevate Your Kitchen",
-        description: "Discover our premium granite countertops, crafted for timeless elegance and durability.",
-        // button: "Explore Kitchen Granite",
-        button: "Explore Collections",
-        image: "https://drive.google.com/uc?id=1MPdh9i3yjAhdQOuiPxtWLUia-u9pl2x4",
-        redirect: uris?.product,
-        position: "left"
-    },
-    {
-        id: 2,
-        title: "Transform Your Bathroom",
-        description: "Create a spa-like retreat with our luxurious granite surfaces.",
-        // button: "View Bathroom Collection",
-        button: "Explore Collections",
-        image: "https://drive.google.com/uc?id=17pWZWGxjqmyNjk9gBbOq9tkmB9OMX8ug",
-        redirect: uris?.product,
-        position: "right"
-    },
-    {
-        id: 3,
-        title: "Enhance Your Outdoor Spaces",
-        description: "From patios to garden paths, explore our durable and stylish granite options.",
-        // button: "Discover Outdoor Granite",
-        button: "Explore Collections",
-        image: "https://drive.google.com/uc?id=1K6AZDfN8vs2r69dcaVdhYLBNZJJD_JyF",
-        redirect: uris?.product,
-        position: "left"
-    },
-    {
-        id: 4,
         title: "Discover Rare Beauty",
         description: "Explore our collection of exotic and unique granite for distinctive projects.",
         // button: "Browse Exotic Granite",
         button: "Explore Collections",
-        image: "https://drive.google.com/uc?id=1zj30VbkFO1FpM1YfehOAj_Gr3KPq_bUg",
+        image: Exotic,
         redirect: uris?.product,
         position: "right"
+    },
+    {
+        id: 2,
+        title: "Elevate Your Kitchen",
+        description: "Discover our premium granite countertops, crafted for timeless elegance and durability.",
+        // button: "Explore Kitchen Granite",
+        button: "Explore Collections",
+        image: Countertops,
+        redirect: uris?.product,
+        position: "left"
+    },
+    {
+        id: 3,
+        title: "Transform Your Bathroom",
+        description: "Create a spa-like retreat with our luxurious granite surfaces.",
+        // button: "View Bathroom Collection",
+        button: "Explore Collections",
+        image: Bathroom,
+        redirect: uris?.product,
+        position: "right"
+    },
+    {
+        id: 4,
+        title: "Enhance Your Outdoor Spaces",
+        description: "From patios to garden paths, explore our durable and stylish granite options.",
+        // button: "Discover Outdoor Granite",
+        button: "Explore Collections",
+        image: Outdoor,
+        redirect: uris?.product,
+        position: "left"
     },
     {
         id: 5,
@@ -56,7 +61,7 @@ const carouselContents = [
         description: "Get custom granite solutions designed to match your unique style and requirements.",
         // button: "Request a Custom Order",
         button: "Explore Collections",
-        image: HeroKitchenCountertop,
+        image: Designer,
         redirect: uris?.product,
         position: "left",
     }
@@ -84,11 +89,11 @@ const HeroSectionMain = () => {
                                     <div className={styles["button"]} onClick={() => handleNavigate(content?.redirect)}>{content?.button}</div>
                                 </div>
                                 <div className={styles["text-background"]}>
-                                    <Image alt="Souza United Export Background" src={NetBg} width={100} height={100} />
+                                    <Image alt="Souza United Export Background" src={NetBg} width={3000} height={3000} />
                                 </div>
                             </div>
                             <div className={styles["carousel-image-section"]}>
-                                <Image alt={content?.title} src={content?.image} width={100} height={100} />
+                                <Image alt={content?.title} src={content?.image} width={5000} height={5000} />
                             </div>
                         </div>
                     )

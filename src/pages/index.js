426bcd1147624eebd63a89a9cda1a9716505a61e
floyd-esgds <floyd.dsouza@esgds.io>
2024-10-18 20:24:16 +0530

    Add description & sitemap

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <Head>
         <title>Souza United Exports</title>
-        <meta name="description" content="Souza United Exports - Premium granite from Karkala, India. We offer sustainably sourced, high-quality granite for construction, interiors, and custom projects. Elevate your designs with our expertly crafted stone." />
+        <meta name="description" content="Souza United Exports - Premium granite supplier from Karkala, India. We offer sustainably sourced, high-quality granite slabs for construction, interiors, and custom projects. Contact us today to elevate your designs with our expertly crafted stone." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
